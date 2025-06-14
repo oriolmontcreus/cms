c015b42bfe6bd29618e46f30b0dab4370a1d0cc0
oriolmontcreus <oriolmontcreus@gmail.com>
2025-06-14 18:05:21 +0200

    feat: clean install shadcn-svelte lastest

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+
+    let name = 'World';
+</script>
+
 <h1 class="text-3xl font-bold underline bg-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<p>Hello {name}!</p>
+
+<Button onclick={() => name = 'Svelte'}>Click me</Button>
