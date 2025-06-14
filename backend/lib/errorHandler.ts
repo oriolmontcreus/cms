ffbcb2e0f6a52d0e476170a2b5c1a31741a7da2d
oriolmontcreus <oriolmontcreus@gmail.com>
2025-06-14 18:37:17 +0200

    chore: installed necessary packages

--- backend/lib/errorHandler.ts
+++ backend/lib/errorHandler.ts
@@ -1,8 +1,9 @@
-import { Context } from "hono/mod.js";
+import { Context } from "hono";
 import { log } from '@/lib/log.js';
 import ExtendedError from '@/errors/ExtendedError.js';
 import { UNAUTHORIZED } from '@/constants/http.js';
 import { red } from "colors";
+import { ContentfulStatusCode } from "hono/utils/http-status";
 
 export const errorHandler = (err: Error, c: Context) => {
   const source = `${c.req.method} ${new URL(c.req.url).pathname}`;
@@ -20,5 +21,5 @@ export const errorHandler = (err: Error, c: Context) => {
   const statusCode = err instanceof ExtendedError ? err.statusCode : 500;
   const resMsg = err instanceof ExtendedError ? err.message : 'Internal Server Error';
 
-  return c.text(resMsg, statusCode);
+  return c.text(resMsg, statusCode as ContentfulStatusCode);
 };
