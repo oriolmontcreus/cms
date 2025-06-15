1cb27f31d292161e4f320eac12d5ad52734ac9ff
oriolmontcreus <oriolmontcreus@gmail.com>
2025-06-15 19:53:47 +0200

    fix: fixed colors backend error

--- backend/lib/log.ts
+++ backend/lib/log.ts
@@ -1,19 +1,19 @@
-import { cyan, yellow, red, magenta, dim, green } from 'colors';
+import colors from 'colors';
 import { formatTimestamp } from '@/lib/dateUtils.js';
 import { ENV, Environment } from '@/constants/env.js';
 
 export type LogType = 'INFO' | 'WARN' | 'ERROR' | 'UNAUTH' | 'DEBUG';
 
 const color = {
-    'INFO': cyan,
-    'WARN': yellow,
-    'UNAUTH': magenta,
-    'ERROR': red,
-    'DEBUG': green
+    'INFO': colors.cyan,
+    'WARN': colors.yellow,
+    'UNAUTH': colors.magenta,
+    'ERROR': colors.red,
+    'DEBUG': colors.green
 }
 
 export function log(type: LogType = 'INFO', ...msg: unknown[]) {
     if (type !== 'ERROR' && ENV === Environment.PRODUCTION) return;
     const timestamp = formatTimestamp(new Date());
-    console.log(`[${dim(timestamp)}][${color[type](type)}]`, ...msg);
+    console.log(`[${colors.dim(timestamp)}][${color[type](type)}]`, ...msg);
 }
