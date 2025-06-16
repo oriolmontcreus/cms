9d792cc8ef596ffcf2a26f17304930869f92b3d1
oriolmontcreus <oriolmontcreus@gmail.com>
2025-06-16 21:51:10 +0200

    chore: installed svelte-sonner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import { Toaster } from 'svelte-sonner';
 	
 	let { children } = $props();
 </script>
 
+<svelte:head>
+    <title>Froggy</title>
+</svelte:head>
+
+<Toaster richColors />
 {@render children()}
