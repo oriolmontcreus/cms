bdd8dea66aa3dc0011663d59aafe173dae1b5870
oriolmontcreus <oriolmontcreus@gmail.com>
2025-06-17 21:52:31 +0200

    use handleLogin

--- src/lib/components/LoginForm.svelte
+++ src/lib/components/LoginForm.svelte
@@ -4,15 +4,24 @@
     import { Button } from "$lib/components/ui/button/index.js";
     import { cn, type WithElementRef } from "$lib/utils.js";
     import type { HTMLFormAttributes } from "svelte/elements";
+    import { handleLogin } from "@/services/auth.service";
     let {
         ref = $bindable(null),
         class: className,
         ...restProps
     }: WithElementRef<HTMLFormAttributes> = $props();
     const id = $props.id();
+
+    let email = $state("");
+    let password = $state("");
+
+    async function onSubmit(e: SubmitEvent) {
+        e.preventDefault();
+        await handleLogin(email, password, true);
+    }
 </script>
 
-<form class={cn("flex flex-col gap-6", className)} bind:this={ref} {...restProps}>
+<form class={cn("flex flex-col gap-6", className)} bind:this={ref} {...restProps} onsubmit={onSubmit}>
     <div class="flex flex-col items-center gap-2 text-center">
         <h1 class="text-2xl font-bold">Login to your account</h1>
         <p class="text-muted-foreground text-balance text-sm">
@@ -22,11 +31,11 @@
     <div class="grid gap-6">
         <div class="grid gap-3">
             <Label for="email-{id}">Email</Label>
-            <Input id="email-{id}" type="email" placeholder="m@example.com" required />
+            <Input id="email-{id}" type="email" placeholder="m@example.com" required bind:value={email} />
         </div>
         <div class="grid gap-3">
             <Label for="password-{id}">Password</Label>
-            <Input id="password-{id}" type="password" required />
+            <Input id="password-{id}" type="password" required bind:value={password} />
         </div>
         <Button type="submit" class="w-full">Login</Button>
     </div>
