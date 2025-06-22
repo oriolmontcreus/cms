ed509e4fa78d701c74296d1a220a416f56f3d272
oriolmontcreus <oriolmontcreus@gmail.com>
2025-06-22 17:03:29 +0200

    implemented nice scroll

--- src/routes/pages/[slug]/+page.svelte
+++ src/routes/pages/[slug]/+page.svelte
@@ -2,39 +2,35 @@
     import { page } from '$app/state';
     import FormBuilder from '$lib/components/form-builder/FormBuilder.svelte';
     import type { PageConfig } from '$lib/components/form-builder/types';
-    import { handleGetPageBySlug } from '@/services/page.service';
+    import { getPageBySlug } from '@/services/page.service';
     import type { Page } from '@shared/types/pages.type';
     import { onMount } from 'svelte';
     import SiteHeader from '$lib/components/site-header.svelte';
+    import { safeFetch } from "@/lib/utils/safeFetch";
+    import { ScrollArea } from "$lib/components/ui/scroll-area";
 
     let pageData: Page | null = null;
-    let config: PageConfig | null = null;
+    let config: PageConfig | null = null;   
     let loading = true;
     let error: string | null = null;
 
     onMount(async () => {
-        try {
             loading = true;
             const slug = page.params.slug;
-            pageData = await handleGetPageBySlug(slug);
-            
-            if (pageData && pageData.config) {
-                config = pageData.config as PageConfig;
+            const [data, err] = await safeFetch(getPageBySlug(slug));
+            loading = false;
+            if (data) {
+                pageData = data;
+                config = data.config as PageConfig;
             } else {
                 error = 'Page not found or no configuration available';
             }
-        } catch (e) {
-            error = 'Failed to load page';
-            console.error(e);
-        } finally {
-            loading = false;
-        }
     });
 </script>
 
 <SiteHeader title={config?.title || 'Page'} />
-<div class="flex flex-1 flex-col">
-    <div class="@container/main flex flex-1 flex-col gap-2">
+<div class="flex flex-1 flex-col overflow-hidden">
+    <ScrollArea class="@container/main flex flex-1 flex-col gap-2 max-h-[calc(100dvh-80px)]">
         <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
             <div class="px-4 lg:px-6">
                 {#if loading}
@@ -60,5 +56,5 @@
                 {/if}
             </div>
         </div>
-    </div>
+    </ScrollArea>
 </div> 
