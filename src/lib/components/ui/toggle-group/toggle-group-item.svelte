944a529cc9c15bba02655513c14f46c78453fd7b
oriolmontcreus <oriolmontcreus@gmail.com>
2025-06-24 16:21:55 +0200

    minor style change

--- src/lib/components/ui/toggle-group/toggle-group-item.svelte
+++ src/lib/components/ui/toggle-group/toggle-group-item.svelte
@@ -26,7 +26,7 @@
 			variant: ctx.variant || variant,
 			size: ctx.size || size,
 		}),
-		"min-w-0 flex-1 shrink-0 rounded-none shadow-none first:rounded-l-md last:rounded-r-md focus:z-10 focus-visible:z-10 data-[variant=outline]:border-l-0 data-[variant=outline]:first:border-l",
+		"min-w-0 flex-1 shrink-0 rounded-none shadow-none first:rounded-l-md last:rounded-r-md focus:z-10 focus-visible:z-10 data-[variant=outline]:border-l-0 data-[variant=outline]:first:border-l transition-all duration-300 ease-in-out cursor-pointer",
 		className
 	)}
 	{value}
