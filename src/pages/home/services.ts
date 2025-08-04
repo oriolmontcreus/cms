bef5722fd1c4f4d57830daf63f69ad6eb400bdba
oriolmontcreus <oriolmontcreus@gmail.com>
2025-08-04 22:08:24 +0200

    added nested page for testing purpouses

---
+++ src/pages/home/services.ts
@@ -0,0 +1,8 @@
+import type { PageConfig } from '../../lib/components/form-builder/types';
+
+export const config: PageConfig = {
+    title: "Services",
+    slug: "services",
+    parentSlug: "home",
+    components: []
+};
