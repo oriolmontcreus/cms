1d1847677522da149bc634b2a895a103251a900b
oriolmontcreus <oriolmontcreus@gmail.com>
2025-08-04 16:24:28 +0200

    little page loading style change

--- src/lib/components/PageLoading.svelte
+++ src/lib/components/PageLoading.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+    import Spinner from "$lib/components/Spinner.svelte";
+</script>
+
 <div class="flex h-screen items-center justify-center">
-    <div class="text-center">
-        <div class="animate-spin rounded-full h-8 w-8 border-b-2 border-primary mx-auto mb-4"></div>
-        <p class="text-muted-foreground">Loading...</p>
+    <div class="text-center flex flex-col items-center gap-2">
+        <Spinner />
+        <p class="text-muted-foreground">Loading</p>
     </div>
-</div> 
+</div>
