b93eb10426a9d20dceeff8b7436d923caeec0abd
oriolmontcreus <oriolmontcreus@gmail.com>
2025-08-07 14:59:01 +0200

    fix: fixed import error 500

--- src/lib/components/form-builder/components/index.ts
+++ src/lib/components/form-builder/components/index.ts
@@ -1,3 +1,4 @@
 export { default as ComponentRenderer } from './ComponentRenderer.svelte';
 export { default as UnifiedRenderer } from './UnifiedRenderer.svelte';
-export { default as ResponsiveTabTrigger } from './ResponsiveTabTrigger.svelte'; 
+export { default as ResponsiveTabTrigger } from './ResponsiveTabTrigger.svelte';
+export { default as TranslationModeWrapper } from './TranslationModeWrapper.svelte'; 
