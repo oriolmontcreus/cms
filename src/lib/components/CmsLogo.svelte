f5bb2b92b0ab843bb47d5f9617b7a171a2818aeb
oriolmontcreus <oriolmontcreus@gmail.com>
2025-08-12 16:23:22 +0200

    color change

--- src/lib/components/CmsLogo.svelte
+++ src/lib/components/CmsLogo.svelte
@@ -3,10 +3,8 @@
     export let textSize = "text-xs";
 </script>
 
-<div
-    class="text-white dark:text-black flex size-6 items-center justify-center rounded-md {className}"
->
-    <div class="bg-black dark:bg-white rounded-sm size-6">
+<div class="flex size-6 items-center justify-center rounded-md {className}">
+    <div class="bg-accent dark:bg-accent rounded-sm size-6">
         <span class="{textSize} flex justify-center h-full items-center"
             >EX</span
         >
