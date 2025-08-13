3bc1a4e04f1ccc6cdb988fe1c2745e07dc21d386
oriolmontcreus <oriolmontcreus@gmail.com>
2025-08-13 21:50:20 +0200

    improved checkpoint

--- src/lib/components/form-builder/fields/TextInputWithGlobalVariables.svelte
+++ src/lib/components/form-builder/fields/TextInputWithGlobalVariables.svelte
@@ -307,8 +307,8 @@
             contenteditable="true"
             id={fieldId}
             class={cn(
-                "h-9 w-full rounded-md border border-input bg-transparent text-sm shadow-sm transition-colors",
-                "file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground",
+                "h-9 w-full rounded-md border border-input  text-sm shadow-sm transition-colors bg-background dark:bg-input/30",
+                "file:border-0 file:text-sm file:font-medium file:text-foreground",
                 "placeholder:text-muted-foreground",
                 "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring",
                 "disabled:cursor-not-allowed disabled:opacity-50",
