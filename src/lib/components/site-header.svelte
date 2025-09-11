c5fae41e440a93efc7693e4cb6f427e1fde33fda
oriolmontcreus <oriolmontcreus@gmail.com>
2025-09-11 18:21:34 +0200

    added little animations

--- src/lib/components/site-header.svelte
+++ src/lib/components/site-header.svelte
@@ -5,6 +5,8 @@
 	import ThemeToggle from "./ThemeToggle.svelte";
 	import type { Snippet } from "svelte";
 	import { globalValidationState } from "$lib/stores/validationState";
+	import { scale, fade } from "svelte/transition";
+	import { backOut, cubicOut } from "svelte/easing";
 
 	let { title, children }: { title: string; children?: Snippet } = $props();
 
@@ -24,12 +26,26 @@
 		<div class="relative">
 			<Sidebar.Trigger class="-ml-1" />
 			{#if showValidationBadge}
-				<Badge
-					variant="destructive"
-					class="absolute -top-1 -right-1 h-4 w-4 rounded-full p-0 flex items-center justify-center text-xs font-medium min-w-[16px]"
+				<div
+					in:scale={{ duration: 300, easing: backOut, start: 0.3 }}
+					out:scale={{ duration: 200, easing: cubicOut, start: 0.3 }}
+					class="absolute -top-1 -right-1"
 				>
-					{validationState.errors.length}
-				</Badge>
+					<Badge
+						variant="destructive"
+						class="h-4 w-4 rounded-full p-0 flex items-center justify-center text-xs font-medium min-w-[16px]"
+					>
+						{#key validationState.errors.length}
+							<span
+								in:fade={{ duration: 150, delay: 50 }}
+								out:fade={{ duration: 100 }}
+								class="inline-block"
+							>
+								{validationState.errors.length}
+							</span>
+						{/key}
+					</Badge>
+				</div>
 			{/if}
 		</div>
 		<Separator
